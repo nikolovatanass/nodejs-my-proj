c8e80417d4f5b7470b02a2de30bab395c6b5d54d
nikolovatanass <atanas_nikolov@flutterint.com>
2023-05-23 14:21:42 +0300

    black

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.get('/', (req, res) => {
       <head>
         <style>
           body {
-            background-color: red;
+            background-color: black;
           }
         </style>
       </head>
